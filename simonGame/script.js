54e024ca3fd1024ced3ceb0e764861d369d70c66
Mercy Spectures <71334636+MercySpectures@users.noreply.github.com>
2024-03-18 14:46:46 +0530

    Update script.js

--- simonGame/script.js
+++ simonGame/script.js
@@ -12,34 +12,12 @@ $(document).keypress(function (){
     }
 })
 
-function playSound(name){
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-}
-
-function animatePress(currentColor){
-    $("#" + currentColor).addClass("pressed");
-    setTimeout(() => { $("#" + currentColor).removeClass("pressed"); }, 100);
-}
-
 $(".btn").click(function(){
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
     animatePress(userChosenColor);
 })
-
-function nextSequence(){
-    userClickedPattern = [];
-    level++;
-    $("#level-title").text("Level " + level);
-
-    var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColor);
-}
 
 function checkAnswer(currentLevel){
     if(gamePattern[currentLevel] === userClickedPattern(currentLevel)){
@@ -53,12 +31,34 @@ function checkAnswer(currentLevel){
             playSound("wrong");
             $("body").addClass("game-over");
             setTimeout(function (){
-                document.removeClass("game-over");
+                $("body").removeClass("game-over");
             }, 200);
             $("#level-title").text("Game Over, Press Any Key to Restart");
             startOver();
         }
     }
+}
+
+function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+}
+
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(() => { $("#" + currentColor).removeClass("pressed"); }, 100);
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
 
 function startOver(){
